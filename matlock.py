43e25e89180afad90d50cbc285bce96a2a35945b
Pedro Wightman <pedro.wightman@urosario.edu.co>
2024-11-22 11:49:26 -0500

    Matlock

---
+++ matlock.py
@@ -0,0 +1,43 @@
+import numpy as np
+import csv
+import random
+
+def matlock(mat, puntos):
+    puntos_matlock = [np.linalg.matmul(punto, mat).tolist() for punto in puntos]
+    return puntos_matlock
+
+def matlock_inv(mat, puntos):
+    mat2 = np.linalg.inv(mat)
+    puntos_matlock = [np.linalg.matmul(punto, mat2).tolist() for punto in puntos]
+    return puntos_matlock
+
+
+
+def addRuido(punto, maxRuido, metros: bool = False):
+    if metros:
+        maxRuido = maxRuido*360/4070000
+    puntor = punto.copy()
+    puntor[0] += random.uniform(-maxRuido, maxRuido)
+    puntor[1] += random.uniform(-maxRuido, maxRuido)
+    return puntor
+
+
+
+
+#DATOS
+
+m = [[1, 2, 3], [4, 5, -6], [-7, 2, 9]]
+
+datos = []
+with open('datos.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile )
+    for row in reader:
+        datos.append([float(i) for i in row])
+
+puntos = matlock(m, datos)
+
+print(puntos)
+
+puntos2 = matlock_inv(m, puntos)
+
+print(puntos2)
